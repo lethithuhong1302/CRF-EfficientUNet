178f7f99175638d10c18e2948160fd6970ffaba5
lethithuhong1302 <48873479+lethithuhong1302@users.noreply.github.com>
2021-12-29 19:45:37 +0700

    Create metrics.py

---
+++ metrics.py
@@ -0,0 +1,50 @@
+import keras.backend as K
+import numpy as np
+
+
+def recall(y_true, y_pred): #trùng với sensitivity
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        rc = true_positives / (possible_positives + K.epsilon())
+        return rc
+
+def precision(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        prc= true_positives / (predicted_positives + K.epsilon())
+        return prc
+def IU(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        union=predicted_positives + possible_positives-true_positives
+        iou= true_positives / (union + K.epsilon())
+        return iou
+def f1(y_true, y_pred):
+    prc = precision(y_true, y_pred)
+    rc = recall(y_true, y_pred)
+    return 2*((prc*rc)/(prc+rc+K.epsilon()))
+
+def dice(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2.0 * intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1.0)
+def sensitivity(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
+
+def specificity(y_true, y_pred):
+    true_negatives = K.sum(K.round(K.clip((1-y_true) * (1-y_pred), 0, 1)))
+    possible_negatives = K.sum(K.round(K.clip(1-y_true, 0, 1)))
+    return true_negatives / (possible_negatives + K.epsilon())
+
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    # Flatten
+    y_true_f = tf.reshape(y_true, [-1])
+    y_pred_f = tf.reshape(y_pred, [-1])
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
+    return score
